78902855760aa2128f7f53c971aead3ac436439f
RISHU RAJ GAUTAM <Rishu24oct@gmail.com>
2024-05-13 21:13:59 +0530

    Lecture 01 Updated

--- Chapter 01/01: print.py
+++ Chapter 01/01: print.py
@@ -1,18 +1,28 @@
 # Creating a Hello World program in Python
+# print("Hello World !")
 
 # Multiple Words in Print Statement
+# print("Hello","World","!")
 
 # End Parameter
 # By default, the print() function ends with a newline character (\n). You can change this behavior by specifying the end parameter. For example, you can use the end parameter to print multiple values on the same line:
 
+# print("Hello world",end=",")
+# print("Hello","World","!")
 
 
 # Separator Parameter
-
+# print("Hello","World","!")
+# print("Hello","World","!",sep="")
 
 
 # Single quotes
+# print('Hello world')
 
 # Double quotes
+# print("Hello world")
 
 # Errors with Quotes
+# print("Hello" world")
+print('Hello" world')
+print("Hello' world")
