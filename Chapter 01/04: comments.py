cfde3e9757d1f4aebd14bd0354320b9459d945f5
RISHU RAJ GAUTAM <Rishu24oct@gmail.com>
2024-05-14 00:06:24 +0530

    lecture 05: Comments in Python Update

--- Chapter 01/04: comments.py
+++ Chapter 01/04: comments.py
@@ -4,15 +4,18 @@
 
 # Single-line comments start with a hash (#) symbol and continue until the end of the line. For example:
 
-
+print("Hello world")
 
 # Multi-line comments can be created by enclosing the text within triple quotes (''' or """). This is useful for longer comments or docstrings. For example:
 
-
+"""
+Comments are used to document code and improve its readability. They are ignored by the Python interpreter and are not executed. Comments can be used to explain the purpose of code, provide context, or disable certain parts of the code.
+"""
 
 # Comments can also be used to disable certain parts of the code temporarily. This can be useful for debugging or testing different sections of the code. For example:
 
-# print("Hello, World!")
+# This block of code prints hello world in terminal
+print("Hello, World!")
 # print("This Above program is commented out.")
 
 # In this example, the second print statement is commented out, so it will not be executed when the code is run.
