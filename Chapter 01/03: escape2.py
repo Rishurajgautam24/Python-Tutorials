fd7ae6b890b85ea3c02b04c4613d0eec546e48e4
RISHU RAJ GAUTAM <Rishu24oct@gmail.com>
2024-05-13 22:54:36 +0530

    Lecture 04 updated

--- Chapter 01/03: escape2.py
+++ Chapter 01/03: escape2.py
@@ -1,11 +1,21 @@
 # Escape Charatcer as Normal Text
+print("Hello \" world")
 
 # \\ Backslash (\)
+
+print("Hello \\ world")
+
+# print("Hello \\" world")
 
 
 
 # \\ is used to represent a single backslash character within a string.
 # For example:
+print("Hello \\\" world")
+
 
 # Triple Backslash
 # \\\' and \\\"
+
+print('Hello \\\' world')
+
